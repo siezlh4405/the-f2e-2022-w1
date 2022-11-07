1d1ebbe8c9d6de0c683976df0dd586f60fdce92e
henry.shih <siezlh4405@outlook.com>
2022-11-08 02:11:54 +0800

    feat: 建立 home - banner component & 處理主標題

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './pages/home-page/home-page.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 
 
 @NgModule({
   declarations: [
-    HomePageComponent
+    HomePageComponent,
+    BannerComponent
   ],
   imports: [
     CommonModule
-  ]
+  ],
 })
 export class HomeModule { }
