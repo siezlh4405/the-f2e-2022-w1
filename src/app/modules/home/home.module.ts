49ec3c6817dd9a34f26151b194743df93ea320d3
henry.shih <siezlh4405@outlook.com>
2022-11-10 22:18:29 +0800

    feat: 建立 problem component

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { BannerComponent } from './components/banner/banner.component';
+import { ProblemComponent } from './components/problem/problem.component';
 
 
 
 @NgModule({
   declarations: [
     HomePageComponent,
-    BannerComponent
+    BannerComponent,
+    ProblemComponent
   ],
   imports: [
     CommonModule
